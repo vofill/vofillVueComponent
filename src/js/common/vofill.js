10a9c1538e661cc90f99604b8699644bbbbc499f
vofill <517202113@qq.com>
2020-06-24 00:11:52 +0800

    11

--- src/js/common/vofill.js
+++ src/js/common/vofill.js
@@ -120,6 +120,18 @@ var vofill = {
             }    
         }    
         return tree; 
+    },
+    /**JS groupby函数 */
+    groupby(array, fun) {
+        let groups = {};
+        array.forEach(function(o) {
+            let group = JSON.stringify(fun(o));
+            groups[group] = groups[group] || [];
+            groups[group].push(o);
+        });
+        return Object.keys(groups).map(function(group) {
+            return groups[group];
+        });
     }
 }
 
--- src/view/LRSelect.vue
+++ src/view/LRSelect.vue
@@ -13,10 +13,13 @@
 <template>
     <div>
         <div class="rsl-box">
-            <vofill-lrselect :type=1 :sel-obj="selObj" sel-key="SelIds"></vofill-lrselect>
+            <vofill-lrselect :type=1 :list="list" :sel-obj="selObj" sel-key="SelIds"></vofill-lrselect>
         </div>
         <div class="rsl-box">
-            <vofill-lrselect :type=2 :sel-obj="selObj" sel-key="SelIds"></vofill-lrselect>
+            <vofill-lrselect :type=2 :list="list" :sel-obj="selObj" sel-key="SelIds"></vofill-lrselect>
+        </div>
+        <div class="rsl-box">
+            <vofill-lrselect :type=3 :list="list2" :sel-obj="selObj" sel-key="SelIds"></vofill-lrselect>
         </div>
     </div>
 </template>
@@ -34,7 +37,11 @@
         },
         data () {
             return {
-                selObj: {SelIds: "4,5,6"}
+                list: [{Id: 1, Name: "选项1"}, {Id: 2, Name: "选项2"}, {Id: 3, Name: "选项3"},{Id: 4, Name: "选项4"}, {Id: 5, Name: "选项5"}, {Id: 6, Name: "选项6"}],
+                list2: [{Id: 1, Name: "选项1", DeptId: 1, DeptName: "部门1"}, {Id: 2, Name: "选项2", DeptId: 1, DeptName: "部门1"},
+                        {Id: 3, Name: "选项3", DeptId: 1, DeptName: "部门1"}, {Id: 4, Name: "选项4", DeptId: 1, DeptName: "部门1"},
+                        {Id: 5, Name: "选项5", DeptId: 2, DeptName: "部门2"}, {Id: 6, Name: "选项6", DeptId: 2, DeptName: "部门2"}],
+                selObj: {SelIds: "4,5"}
             }
         },
         created() {
